87556b23ec43c31fc68bb7db137628b3b6da5eb2
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-07 14:33:42 +0900

    feat: Implement Todo Component

---
+++ src/features/todo/components/TodoList.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useTodo } from "../contexts/TodoContext";
+
+const TodoList = () => {
+  const { state, dispatch } = useTodo();
+
+  const handleToggle = () => {};
+  return (
+    <div>
+      {state.length > 0 ? (
+        <div>
+          {state.map((todo) => (
+            <li key={todo.id}>
+              <span onClick={() => dispatch({ type: "toggle", id: todo.id })}>{todo.text}</span>
+              <button onClick={() => dispatch({ type: "remove", id: todo.id })}>削除</button>
+            </li>
+          ))}
+          <div>
+            <button onClick={() => dispatch({ type: "clear" })}>全削除</button>
+          </div>
+        </div>
+      ) : (
+        <p>タスクが空です</p>
+      )}
+    </div>
+  );
+};
+
+export default TodoList;
